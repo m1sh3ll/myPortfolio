dbbfa15b2d5e2acdefed022c1ba6931f2b115b9c
Michelle Nesbitt <mn218@nerdymail.com>
2023-09-06 14:21:39 -0400

    Added Kotlin guessing game

--- My-Learning-Practice/Other/Kotlin/MyApplication/app/src/main/java/com/ftpzero/myapplication/MainActivity.kt
+++ My-Learning-Practice/Other/Kotlin/MyApplication/app/src/main/java/com/ftpzero/myapplication/MainActivity.kt
@@ -3,11 +3,16 @@ package com.ftpzero.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Button
+import android.view.View
+import android.widget.EditText
+import java.lang.Exception
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
     var answer = 0
+    var numOfAttempts = 0
     var isGameOver:Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,18 +25,66 @@ class MainActivity : AppCompatActivity() {
         answer = Random.nextInt(1,25)
     }
 
-    fun btnStartOverTapped(){
+    fun startOver(){
         isGameOver = false
         generateAnswer()
+        numOfAttempts = 0
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Guess 1 to 25"
 
         val answerTextView = findViewById<TextView>(R.id.answer)
         answerTextView.setText("??")
+
     }
 
-    fun btnSubmitTapped() {
-        isGameOver = true
-        val answerTextView = findViewById<TextView>(R.id.answer)
-        answerTextView.setText(answer.toString())
+    fun btnStartOverTapped(view: View){
+       startOver()
+    }
+
+    fun btnSubmitTapped(view: View) {
+        val guess = getUsersGuess() ?: -999
+        val textView = findViewById<TextView>(R.id.textView)
+        if (guess !in 1..25){
+            textView.text = "Guess must be 1 to 25"
+            return
+        }
+
+        var message = ""
+        numOfAttempts++
+
+        if(guess == answer) {
+            message = "Correct! Answer $answer - Guesses: $numOfAttempts"
+            isGameOver = true
+            val answerTextView = findViewById<TextView>(R.id.answer)
+            answerTextView.setText(message)
+
+            val submitButton = findViewById<Button>(R.id.buttonSubmit)
+            submitButton.isEnabled = false
+        }
+        else {
+            message = if (guess < answer) "Too low!" else "Too high!"
+
+            val answerTextView = findViewById<TextView>(R.id.answer)
+            answerTextView.setText(message)
+
+        }
+
+
+    }
+
+    fun getUsersGuess() : Int? {
+        val editTextGuess= findViewById<EditText>(R.id.editTextGuess)
+        val usersGuess = editTextGuess.text.toString()
+        var guessAsInt = 0
+
+        try {
+            guessAsInt = usersGuess.toInt()
+        } catch (e: Exception) {
+            return null
+        }
+
+        return guessAsInt
+
     }
 
 }
