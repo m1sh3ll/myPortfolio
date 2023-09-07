6f41ac4ce49da54394c3a7bd1f42e64590edcc8e
Michelle Nesbitt <mn218@nerdymail.com>
2023-09-07 15:05:01 -0400

    Added some certs

---
+++ My-Learning-Practice/Other/Kotlin/MyAppTest/src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val a = 21
+
+    if (a < 20)
+        if(a > 10)
+            print("Hi $a")
+        else if(a < 10)
+            print ("Bonjour $a")
+        else
+            print ("Welcome $a")
+}
