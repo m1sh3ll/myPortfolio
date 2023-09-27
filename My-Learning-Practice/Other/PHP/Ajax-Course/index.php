c187e4e3636dcefbc89f557665b598649379b660
Michelle Nesbitt <mn218@nerdymail.com>
2023-09-27 08:13:05 -0400

    Add Python and PHP

---
+++ My-Learning-Practice/Other/PHP/Ajax-Course/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <title>Build an API</title>
+</head>
+
+<body>
+  <input type="text" name="name" id="name" value="tester">
+
+  <script src=" https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+  <script>
+      $(function () {
+        console.dir(document);
+
+      })
+  </script>
+</body>
+
+</html>
