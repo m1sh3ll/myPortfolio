34201730293665ebb8fb1c61fcbc934b9e2e3c14
Michelle Nesbitt <mn218@nerdymail.com>
2023-10-08 07:56:27 -0400

    Update Portfolio

--- My-Learning-Practice/Nu-Python/2-SQL/week1/workshop1.sql
+++ My-Learning-Practice/Nu-Python/2-SQL/week1/workshop1.sql
@@ -42,7 +42,64 @@ CREATE TABLE categories (
 );
 
 -- TODO create more tables here...
+CREATE TABLE suppliers (
+    id SERIAL,
+    name TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE customers (
+    id SERIAL,
+    company_name TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE employees (
+    id SERIAL,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE orders (
+    id SERIAL,
+    date date,
+    customer_id INT NOT NULL,
+    employee_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE orders_products (
+    product_id INT NOT NULL,
+    order_id INT NOT NULL,
+    quantity INT NOT NULL,
+    discount NUMERIC NOT NULL,
+    PRIMARY KEY (product_id, order_id)
+);
+
+CREATE TABLE territories (
+    id SERIAL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees_territories (
+    employee_id INT NOT NULL,
+    territory_id INT NOT NULL,    
+    PRIMARY KEY (employee_id, territory_id)
+);
+
+CREATE TABLE offices (
+    id SERIAL,
+    address_line TEXT NOT NULL,
+    territory_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE us_states (
+    id SERIAL,
+    name TEXT NOT NULL,
+    abbreviation CHARACTER(2) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 ---
 --- Add foreign key constraints
@@ -55,6 +112,48 @@ ADD CONSTRAINT fk_products_categories
 FOREIGN KEY (category_id) 
 REFERENCES categories (id);
 
-
 -- TODO create more constraints here...
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id)
+REFERENCES customers (id);
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_employees
+FOREIGN KEY (employee_id)
+REFERENCES employees (id);
+
+ALTER TABLE products
+ADD CONSTRAINT fk_products_suppliers 
+FOREIGN KEY (supplier_id) 
+REFERENCES suppliers (id);
+
+ALTER TABLE orders_products
+ADD CONSTRAINT fk_orders_products_orders 
+FOREIGN KEY (order_id) 
+REFERENCES orders (id);
+
+ALTER TABLE orders_products
+ADD CONSTRAINT fk_orders_products_products 
+FOREIGN KEY (product_id) 
+REFERENCES products (id);
+
+
+ALTER TABLE employees_territories
+ADD CONSTRAINT fk_employees_territories_employees
+FOREIGN KEY (employee_id) 
+REFERENCES employees (id);
+
+ALTER TABLE employees_territories
+ADD CONSTRAINT fk_employees_territories_territories
+FOREIGN KEY (territory_id) 
+REFERENCES territories (id);
+
+ALTER TABLE offices
+ADD CONSTRAINT fk_offices_territories
+FOREIGN KEY (territory_id)
+REFERENCES territories (id);
+
+
 
--- My-Learning-Practice/Other/Java/SpringBoot/mycoolapp/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ My-Learning-Practice/Other/Java/SpringBoot/mycoolapp/mycoolapp/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,3 +1,5 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
